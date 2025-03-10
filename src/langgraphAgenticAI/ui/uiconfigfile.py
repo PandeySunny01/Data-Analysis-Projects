1e4068206bd4249466183bf9801ae104aaea011c
Sunny Pandey <p.sunny.0622@gmail.com>
2025-03-11 00:32:04 +0530

    load ui config and folder structure

---
+++ src/langgraphAgenticAI/ui/uiconfigfile.py
@@ -0,0 +1,15 @@
+from configparser import ConfigParser
+
+class Config:
+    def __init__(self, config_file="src\langgraphAgenticAI\ui\uiconfigfile.py"):
+        self.config=ConfigParser()
+        self.config.read(config_file)
+    
+    def get_llm_option(self):
+        return self.config["DEFAULT"].get("LLM_OPTIONS").split(", ") 
+    def get_usecase_option(self):
+        return self.config["DEFAULT"].get("USECASE_OPTIONS").split(", ") 
+    def get_groqmodel_option(self):
+        return self.config["DEFAULT"].get("GROQ_MODEL_OPTIONS").split(", ") 
+    def get_pagetitle_option(self):
+        return self.config["DEFAULT"].get("PAGE_TITLE").split(", ") 
