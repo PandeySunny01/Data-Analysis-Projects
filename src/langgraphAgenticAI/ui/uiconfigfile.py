bb6afda7c12a389cc5b02c86a3fbe6615406f171
Sunny Pandey <p.sunny.0622@gmail.com>
2025-03-13 09:33:42 +0530

    streamlit app

--- src/langgraphAgenticAI/ui/uiconfigfile.py
+++ src/langgraphAgenticAI/ui/uiconfigfile.py
@@ -1,10 +1,9 @@
 from configparser import ConfigParser
 
 class Config:
-    def __init__(self, config_file="src\langgraphAgenticAI\ui\uiconfigfile.py"):
+    def __init__(self, config_file="./src/langgraphagenticai/ui/uiconfigfile.ini"):
         self.config=ConfigParser()
         self.config.read(config_file)
-    
     def get_llm_option(self):
         return self.config["DEFAULT"].get("LLM_OPTIONS").split(", ") 
     def get_usecase_option(self):
