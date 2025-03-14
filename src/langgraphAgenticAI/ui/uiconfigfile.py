f0306f96fad75091d2fbd467aabb5650ffec0107
Sunny Pandey <p.sunny.0622@gmail.com>
2025-03-14 20:08:43 +0530

    debug3

--- src/langgraphAgenticAI/ui/uiconfigfile.py
+++ src/langgraphAgenticAI/ui/uiconfigfile.py
@@ -5,11 +5,26 @@ class Config:
         self.config=ConfigParser()
         self.config.read(config_file)
     def get_llm_option(self):
-        return self.config["DEFAULT"].get("LLM_OPTIONS").split(", ")
+        if type(self.config["DEFAULT"].get("LLM_OPTIONS"))==None:
+            return self.config["DEFAULT"].get("LLM_OPTIONS")
+        else:
+            return self.config["DEFAULT"].get("LLM_OPTIONS").split(", ")
     def get_usecase_option(self):
-        return self.config["DEFAULT"].get("USECASE_OPTIONS").split(", ") 
+        if type(self.config["DEFAULT"].get("USECASE_OPTIONS"))==None:
+            return self.config["DEFAULT"].get("USECASE_OPTIONS")
+        else:
+            return self.config["DEFAULT"].get("USECASE_OPTIONS").split(", ") 
+        
     def get_groqmodel_option(self):
-        return self.config["DEFAULT"].get("GROQ_MODEL_OPTIONS").split(", ") 
+        if type(self.config["DEFAULT"].get("GROQ_MODEL_OPTIONS"))==None:
+            return self.config["DEFAULT"].get("GROQ_MODEL_OPTIONS")
+        else:
+            return self.config["DEFAULT"].get("GROQ_MODEL_OPTIONS").split(", ")  
+        
     def get_pagetitle_option(self):
-        # return self.config["DEFAULT"].get("PAGE_TITLE").split(", ") 
-        return self.config["DEFAULT"].get("PAGE_TITLE")
+        if type(self.config["DEFAULT"].get("PAGE_TITLE"))==None:
+            return self.config["DEFAULT"].get("PAGE_TITLE")
+        else:
+            return self.config["DEFAULT"].get("PAGE_TITLE").split(", ")   
+        
+        
